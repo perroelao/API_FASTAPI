c2bd2ec5fb7b11a12f77555564258aae33db1b3e
perroelao <jo.cisterna@duocuc.cl>
2025-05-16 21:48:43 -0400

    API terminada

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import APIRouter,HTTPException
 from app.database import get_conexion
 
@@ -124,23 +125,110 @@ def agregar_usuario(id_usuario:int, rut:str, nombre:str, apellido_p:str, apellid
         raise HTTPException(status_code=500, detail=str(ex))
     
 @router.put("/{id_actualizar}")
-def actualizar_usuario():
+def actualizar_usuario(id_actualizar:int, rut:str, nombre:str, apellido_p:str, apellido_m:str, snombre:str, email:str, fono:str, direccion:str, password:str, rol_id:int):
     try:
-        pass
+        cone = get_conexion()
+        cursor = cone.cursor()
+        cursor.execute("""
+            UPDATE USUARIO 
+            SET rut = :rut,
+                nombre = :nombre,
+                apellido_p = :apellido_p,
+                apellido_m = :apellido_m,
+                snombre = :snombre,
+                email = :email,
+                fono = :fono,
+                direccion = :direccion,
+                password = :password,
+                rol_id = :rol_id
+            WHERE id_usuario = :id_usuario
+        """, {
+            "rut": rut,
+            "nombre": nombre,
+            "apellido_p": apellido_p,
+            "apellido_m": apellido_m,
+            "snombre": snombre,
+            "email": email,
+            "fono": fono,
+            "direccion": direccion,
+            "password": password,
+            "rol_id": rol_id,
+            "id_usuario": id_actualizar
+        })
+        cone.commit()
+        cursor.close()
+        cone.close()
+        return {"mensaje": "Usuario actualizado correctamente"}
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
     
 @router.delete("/{id_eliminar}")
-def eliminar_usuario():
+def eliminar_usuario(id_eliminar:int):
     try:
-        pass
+        cone = get_conexion()
+        cursor = cone.cursor()
+        cursor.execute("""DELETE FROM USUARIO 
+                       WHERE id_usuario = :id_usuario""", 
+                       {"id_usuario": id_eliminar})
+        cone.commit()
+        cursor.close()
+        cone.close()
+        return {"mensaje": "Usuario eliminado correctamente"}
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
 
 @router.patch("/{id_actualizar}")
-def actualizar_patch():
+def actualizar_patch(id_actualizar:int, rut:Optional[str]=None, nombre:Optional[str]=None, apellido_p:Optional[str]=None, apellido_m:Optional[str]=None, snombre:Optional[str]=None, email:Optional[str]=None, fono:Optional[str]=None, direccion:Optional[str]=None, password:Optional[str]=None, rol_id:Optional[int]=None):
     try:
-        pass
+        if not rut and not nombre and not apellido_p and not apellido_m and not snombre and not email and not fono and not direccion and not password and not rol_id:
+            raise HTTPException(status_code=400, detail="No se han proporcionado datos para actualizar")
+        cone = get_conexion()
+        cursor = cone.cursor()
+        campos=[]
+        valores={"id_usuario": id_actualizar}
+        if rut:
+            campos.append("rut = :rut")
+            valores["rut"] = rut
+        if nombre:
+            campos.append("nombre = :nombre")
+            valores["nombre"] = nombre
+        if apellido_p:
+            campos.append("apellido_p = :apellido_p")
+            valores["apellido_p"] = apellido_p
+        if apellido_m:
+            campos.append("apellido_m = :apellido_m")
+            valores["apellido_m"] = apellido_m
+        if snombre:
+            campos.append("snombre = :snombre")
+            valores["snombre"] = snombre
+        if email:
+            campos.append("email = :email")
+            valores["email"] = email
+        if fono:    
+            campos.append("fono = :fono")
+            valores["fono"] = fono
+        if direccion:
+            campos.append("direccion = :direccion")
+            valores["direccion"] = direccion
+        if password:
+            campos.append("password = :password")
+            valores["password"] = password
+        if rol_id:
+            campos.append("rol_id = :rol_id")
+            valores["rol_id"] = rol_id
+        cursor.execute(F"""
+            UPDATE USUARIO 
+            SET {', '.join(campos)}
+            WHERE id_usuario = :id_usuario
+        """,valores)
+        if cursor.rowcount == 0:
+            cone.close()
+            cursor.close()
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+        cone.commit()
+        cursor.close()
+        cone.close()
+        return {"mensaje": "Usuario actualizado correctamente"}
     except Exception as ex:
         raise HTTPException(status_code=500, detail=str(ex))
     
