fa9efd4d300df2302bd0381f763fd1bb23bd29ba
perroelao <jo.cisterna@duocuc.cl>
2025-05-29 19:32:09 -0400

    agregado el middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.routers import usuarios
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API gestion de usuarios",
     version="1.0.0",
     description="API para gestionar usuarios usando FASTAPI y ORACLE"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(usuarios.router)
